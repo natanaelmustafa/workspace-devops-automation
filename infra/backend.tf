abe642161756f810494f9a707b8856587853de78
natanmustafa <natanaelmustafa@gmail.com>
2026-01-15 17:35:01 -0300

    Segundo commit

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
         resource_group_name = "rg-tfstate"
-        storage_account_name = "tfstatecurso4295"
+        storage_account_name = "tfstatecurso3323"
         container_name = "tfstate"
         key = "terraform.tfstate"
     }
